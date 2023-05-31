63fe6b925a0cc296fd3c4bb814d8cd0cba15e29f
Sebastian Kopf <sebastian.kopf@colorado.edu>
2023-05-31 14:23:19 -0600

    update figures

--- scripts/stats_functions.R
+++ scripts/stats_functions.R
@@ -1,46 +1,3 @@
-#' generate a york regression line based on 
-#' @param ds data frame with york regression parameter columns 'a', 'b', 'cov_ab', 'sigma_a', 'sigma_b', 'xmin', 'xmax'
-#' @param expand how far to expand the line beyond the data's xmin/xmax (see [dplyr::expansion]), default is 10%
-generate_york_regression_line <- function(ds, expand = ggplot2::expansion(mult = 0.1), n = 10L) {
- 
-  expand_range <- function(min, max, mult, add, sign) {
-    if (sign < 0) min - (max - min) * mult - add
-    else max + (max - min) * mult + add
-  }
-  calc_x_from_y <- function(y, a, b) (y - a)/b
-  
-  # not used yet
-  # calc_x_with_ci <- function(y, a, b) {
-  #   (cov_ab + b * y - a * b + sqrt( (a * b - b * y - cov_ab)^2 - (b^2 - sb^2) * (a^2 - 2 * a * y - sa^2 + y2) ) ) / (b^2 - sb^2)
-  # }
-  
-  ds |>
-    dplyr::mutate(
-      # xmin
-      yrl_xmin1 = expand_range(xmin, xmax, expand[1], expand[2], -1),
-      yrl_xmin2 = expand_range(calc_x_from_y(ymin, a, b), calc_x_from_y(ymax, a, b), expand[1], expand[2], -1),
-      yrl_xmin = ifelse(yrl_xmin1 > yrl_xmin2, yrl_xmin1, yrl_xmin2),
-      # xmax
-      yrl_xmax1 = expand_range(xmin, xmax, expand[3], expand[4], +1),
-      yrl_xmax2 = expand_range(calc_x_from_y(ymin, a, b), calc_x_from_y(ymax, a, b), expand[3], expand[4], +1),
-      yrl_xmax = ifelse(yrl_xmax1 < yrl_xmax2, yrl_xmax1, yrl_xmax2)
-    ) |>
-    dplyr::cross_join(data.frame(yrl_i = 1:n)) |>
-    dplyr::mutate(
-      # y = a + x * b --> x = (y - a) / b
-      # sigma_y = sqrt(sa^2 + sb^2 * x^2 + 2 * x * cov_ab)
-      # sigma_x = (y - a) / b * sqrt(sa^2/(y-a)^2 + sb^2 / b^2 + 2 * 1/b * 1/(y-a) * cov)
-      # ymax = a + x * b + sqrt(sa^2 + sb^2 * x^2 + 2 * x * cov)
-      # ymin = a + x * b - sqrt(sa^2 + sb^2 * x^2 + 2 * x * cov) 
-      # --> x = (cov + b * y - a * b +/- sqrt( (a*b - b*y - cov)^2 - (b^2 - sb^2) * (a^2 - 2 * a * y - sa^2 + y^2) ) ) / (b^2 - sb^2)
-      yrl_x = yrl_xmin + (yrl_i - 1L) * (yrl_xmax - yrl_xmin) / (!!n - 1L),
-      yrl_y = a + b * yrl_x,
-      yrl_sigma_y = sqrt(sigma_a^2 + (sigma_b * yrl_x)^2 + 2 * yrl_x * cov_ab),
-      yrl_sigma_x = abs((yrl_y - a) / b) * sqrt( (sigma_a/(yrl_y - a))^2 + (sigma_b/b)^2 + 2 * 1/b * 1/(yrl_y - a) * cov_ab)
-    )
-}
-
-
 #' York regression
 #' 
 #' Adapted from the IsoplotR::york and geostats::york implementations which are direct translations of York, D., Evensen, N.M., Martı́nez, M.L., De Basabe Delgado, J. (2004) Unified equations for the slope, intercept, and standard errors of the best straight line. American Journal of Physics 72, 367–375.
@@ -155,4 +112,50 @@ york_regression <- function (X, Y, sigma_X, sigma_Y, omega_X = 1/sigma_X^2, omeg
   )
   
   return(retval)
+}
+
+#' generate a york regression line based on 
+#' @param ds data frame with york regression parameter columns 'a', 'b', 'cov_ab', 'sigma_a', 'sigma_b', 'xmin', 'xmax'
+#' @param expand how far to expand the line beyond the data's xmin/xmax (see [dplyr::expansion]), default is 10%
+generate_york_regression_line <- function(ds, expand = ggplot2::expansion(mult = 0.1), n = 10L) {
+  
+  expand_range <- function(min, max, mult, add, sign) {
+    if (sign < 0) min - (max - min) * mult - add
+    else max + (max - min) * mult + add
+  }
+  calc_x_from_y <- function(y, a, b) (y - a)/b
+  
+  # not used yet
+  # calc_x_with_ci <- function(y, a, b) {
+  #   (cov_ab + b * y - a * b + sqrt( (a * b - b * y - cov_ab)^2 - (b^2 - sb^2) * (a^2 - 2 * a * y - sa^2 + y2) ) ) / (b^2 - sb^2)
+  # }
+  
+  ds |>
+    dplyr::mutate(
+      # xmin
+      yrl_xmin_x = expand_range(xmin, xmax, expand[1], expand[2], -1),
+      yrl_xmin_y = expand_range(calc_x_from_y(ymin, a, b), calc_x_from_y(ymax, a, b), expand[1], expand[2], -1),
+      # xmax
+      yrl_xmax_x = expand_range(xmin, xmax, expand[3], expand[4], +1),
+      yrl_xmax_y = expand_range(calc_x_from_y(ymin, a, b), calc_x_from_y(ymax, a, b), expand[3], expand[4], +1),
+      # is this always the right choice?
+      yrl_xmin = yrl_xmin_y, # is this always the right choice?
+      yrl_xmax = yrl_xmax_y
+      #yrl_xmin = ifelse(yrl_xmin1 > yrl_xmin2, yrl_xmin1, yrl_xmin2),
+      #yrl_xmax = ifelse(yrl_xmax_x < yrl_xmax_y, yrl_xmax_x, yrl_xmax_y)
+    ) |>
+    dplyr::select(-starts_with("yrl_xmin_"), -starts_with("yrl_xmax_")) |>
+    dplyr::cross_join(data.frame(yrl_i = 1:n)) |>
+    dplyr::mutate(
+      # y = a + x * b --> x = (y - a) / b
+      # sigma_y = sqrt(sa^2 + sb^2 * x^2 + 2 * x * cov_ab)
+      # sigma_x = (y - a) / b * sqrt(sa^2/(y-a)^2 + sb^2 / b^2 + 2 * 1/b * 1/(y-a) * cov)
+      # ymax = a + x * b + sqrt(sa^2 + sb^2 * x^2 + 2 * x * cov)
+      # ymin = a + x * b - sqrt(sa^2 + sb^2 * x^2 + 2 * x * cov) 
+      # --> x = (cov + b * y - a * b +/- sqrt( (a*b - b*y - cov)^2 - (b^2 - sb^2) * (a^2 - 2 * a * y - sa^2 + y^2) ) ) / (b^2 - sb^2)
+      yrl_x = yrl_xmin + (yrl_i - 1L) * (yrl_xmax - yrl_xmin) / (!!n - 1L),
+      yrl_y = a + b * yrl_x,
+      yrl_sigma_y = sqrt(sigma_a^2 + (sigma_b * yrl_x)^2 + 2 * yrl_x * cov_ab),
+      yrl_sigma_x = abs((yrl_y - a) / b) * sqrt( (sigma_a/(yrl_y - a))^2 + (sigma_b/b)^2 + 2 * 1/b * 1/(yrl_y - a) * cov_ab)
+    )
 }
